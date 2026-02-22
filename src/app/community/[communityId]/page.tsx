5afd0119670df5202e98001d7ae5ca66b2d1a531
ShohbozKurbonbekov <skurbonbekov98@gmail.com>
2026-02-22 11:46:59 +0900

    feat(setup-router-check): Initial setup & router testing

---
+++ src/app/community/[communityId]/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function CommunityDetail() {
+  return <div>CommunityDetail</div>;
+}
---
+++ src/app/property/[propertyId]/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function PropertyDetail() {
+  return <div>PropertyDetail</div>;
+}
